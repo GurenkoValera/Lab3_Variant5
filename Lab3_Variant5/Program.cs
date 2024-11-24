b2626ab504cfdc67cf14820761fb32299b1df830
valer <valer@DESKTOP-QD94NVR>
2024-11-24 18:39:20 +0300

    Добавьте файлы проекта.

---
+++ Lab3_Variant5/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int size = 9;
+        int[,] matrix = new int[size, size];
+        Random random = new Random();
+
+        // Заполнение матрицы случайными числами от 1 до 10
+        for (int i = 0; i < size; i++)
+        {
+            for (int j = 0; j < size; j++)
+            {
+                matrix[i, j] = random.Next(1, 11); // Случайные числа от 1 до 10
+            }
+        }
+
+        // Вычисление сумм элементов в строках и столбцах, и размещение их на главной диагонали
+        for (int i = 0; i < size; i++)
+        {
+            int rowSum = 0;
+            int colSum = 0;
+
+            for (int j = 0; j < size; j++)
+            {
+                rowSum += matrix[i, j]; // Сумма элементов в строке
+                colSum += matrix[j, i]; // Сумма элементов в столбце
+            }
+
+            matrix[i, i] = rowSum + colSum; // Размещение суммы на главной диагонали
+        }
+
+        // Отображение матрицы на экране
+        Console.WriteLine("Матрица:");
+        for (int i = 0; i < size; i++)
+        {
+            for (int j = 0; j < size; j++)
+            {
+                Console.Write($"{matrix[i, j],4} "); // Форматированный вывод
+            }
+            Console.WriteLine();
+        }
+    }
+}
